aa68951f2b706fb7fc74074310a24131b2042108
Andrea Cleland <andreaclelan@gmail.com>
2022-02-14 15:26:35 -0800

    implemented logout and added event listener for the button

--- other-page/other-page.js
+++ other-page/other-page.js
@@ -1,2 +1,10 @@
 // use checkAuth function to redirect is user not authenticated
 // add event listener to the logout button and call logout
+
+import { checkAuth, logout } from '../fetch-utils.js';
+
+const logoutButton = document.getElementById('logout');
+
+logoutButton.addEventListener('click', () => {
+    logout();
+});
